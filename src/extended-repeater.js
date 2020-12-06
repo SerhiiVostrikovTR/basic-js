9de01e40cf54a85bf5007863632d464d7a6c7c49
Serhii_Vostrikov <svostrikov.contractor@libertyglobal.com>
2020-12-06 19:46:01 +0200

    extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(rep_str, rep_options) {
+  let separator;
+  let additionSeparator;
+  let addition;
+  if (rep_options['separator'])
+  {
+    separator = rep_options['separator'];
+  }
+  else
+  {
+    separator = '+';
+  }
+  if (rep_options['additionSeparator']){
+    additionSeparator = rep_options['additionSeparator'];
+  }
+  else {
+    additionSeparator = '|';
+  }
+  if (rep_str !== null){
+    rep_str = rep_str.toString();}
+  if(typeof(rep_options['addition']) !== 'string' && rep_options['addition'] !== undefined && rep_options['addition']!==null)
+  {
+    addition = rep_options['addition'].toString();}
+  else {
+    addition = rep_options['addition']
+  }
+  if (typeof(addition) !== "undefined" ){
+    let addition_str = (addition+additionSeparator).repeat(rep_options['additionRepeatTimes']-1).concat(addition);
+    rep_str = rep_str+addition_str;
+  }
+
+  return (rep_str + separator).repeat(rep_options['repeatTimes'] - 1) + rep_str
+}
+
   
