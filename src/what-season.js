b4561b20db777b682e7b4a7d2e146677c82d083e
Serhii_Vostrikov <svostrikov.contractor@libertyglobal.com>
2020-12-06 15:47:19 +0200

    what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(userDate) {
+  if (typeof userDate === 'undefined' || userDate === null){ return 'Unable to determine the time of year!';}
+  if (Object.prototype.toString.call(userDate) === "[object Date]")
+  {
+    if (isNaN(userDate.getTime())) {
+      throw "Error";
+    }
+  }
+  else {throw 'Unable to determine the time of year!';}
+  let month = userDate.getMonth();
+  console.log(month);
+  let season = '';
+  switch((month+1).toString()) {
+    case '12':
+    case '1':
+    case '2':
+      season = 'winter';
+      break;
+    case '3':
+    case '4':
+    case '5':
+      season = 'spring';
+      break;
+    case '6':
+    case '7':
+    case '8':
+      season = 'summer';
+      break;
+    case '9':
+    case '10':
+    case '11':
+      season = 'fall';
+      break;
+  }
+  return season;
 };
+
