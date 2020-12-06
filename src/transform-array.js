33c5e164dcd7357ea1167f8225a66b51c9313011
Serhii_Vostrikov <svostrikov.contractor@libertyglobal.com>
2020-12-06 16:03:38 +0200

    transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arrayToBeTransformed) {
+  if(!Array.isArray(arrayToBeTransformed)){
+    throw new CustomError('It is not array!');
+  }
+  let result_array = arrayToBeTransformed;
+
+  for (let i=0; i<arrayToBeTransformed.length; i++){
+    if(typeof(arrayToBeTransformed[i]) == 'string'){
+      switch(arrayToBeTransformed[i]){
+        case '--discard-prev':
+          result_array.splice(i,1);
+          if(i!==0){
+            result_array.splice(i-1,1);}
+          break;
+        case '--discard-next':
+          if(i!==arrayToBeTransformed.length-1){
+            result_array.splice(i+1,1);
+          }
+          result_array.splice(i,1);
+          break;
+        case '--double-next':
+          if(i!==arrayToBeTransformed.length-1){
+            result_array.splice(i, 1, arrayToBeTransformed[i+1]);
+          }
+          else {
+            result_array.splice(i,1);
+          }
+          break;
+        case '--double-prev':
+          if(i!==0){
+            result_array.splice(i, 1, arrayToBeTransformed[i-1]);
+          }
+          else {
+            result_array.splice(i,1);
+          }
+          break;
+
+      }
+    }
+  }
+  return result_array
 };
