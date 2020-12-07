0f684c6f52fa36118ec118e846d3c99f3bc29ff6
serhii_vostrikov <serhii.vostrikov@thomsonreuters.com>
2020-12-07 15:53:43 +0200

    simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
+let intermediate_chain = [];
+
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return intermediate_chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value='') {
+    intermediate_chain.push("( " + value +" )");
+    return chainMaker;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(isNaN(position) || position<0 || position>chainMaker.getLength()-1) {
+      intermediate_chain = []; // clear the chain if position incorrect
+      throw CustomError('Incorrect position!')}
+    intermediate_chain.splice(position-1, 1);
+    return chainMaker;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    intermediate_chain.reverse();
+    return chainMaker;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let finalize_chain_value = intermediate_chain.join("~~");
+    intermediate_chain = []; // clear the chain after finishing the chain
+    return finalize_chain_value;
   }
+
 };
 
 module.exports = chainMaker;
