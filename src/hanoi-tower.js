341987df1eebc7590958c65ddca6274ae630619f
Serhii_Vostrikov <svostrikov.contractor@libertyglobal.com>
2020-12-08 01:30:33 +0200

    refactor hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let turns = Math.pow(2, disksNumber) - 1;
+  let turns = 2**disksNumber - 1;
   let seconds = 3600/turnsSpeed*turns;
-  return {'turns': turns, 'seconds': Math.floor(seconds)}
+  return {'turns': turns, 'seconds': Math.floor(seconds)};
 };
 
 
