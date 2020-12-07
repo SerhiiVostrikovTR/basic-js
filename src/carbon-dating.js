4e7de971cff28c83e51486b1188d4d09bacb7ed2
Serhii_Vostrikov <svostrikov.contractor@libertyglobal.com>
2020-12-08 01:26:44 +0200

    refactor carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,9 +4,11 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  console.log(sampleActivity);
-  if (typeof(sampleActivity) !== 'string' || isNaN(Number(sampleActivity)) || Number(sampleActivity)<1 || Number(sampleActivity)>15){return false;}
-
+  if (typeof(sampleActivity) !== 'string' || isNaN(Number(sampleActivity)) || Number(sampleActivity)<1 ||
+      Number(sampleActivity)>15)
+  {
+    return false;
+  }
 
   return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)*HALF_LIFE_PERIOD/Math.log(2));
 };
