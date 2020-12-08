1cec19859936fe76926f100705470950d8d8320a
Serhii_Vostrikov <svostrikov.contractor@libertyglobal.com>
2020-12-08 02:02:54 +0200

    refactor what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,39 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(userDate) {
-  if (typeof userDate === 'undefined' || userDate === null){ return 'Unable to determine the time of year!';}
-  if (Object.prototype.toString.call(userDate) === "[object Date]")
+  if (typeof(userDate) === 'undefined' || userDate === null)
   {
-    if (isNaN(userDate.getTime())) {
-      throw "Error";
+    return 'Unable to determine the time of year!';
+  }
+  if (this.toString.call(userDate) === '[object Date]')
+  {
+    if (isNaN(userDate.getTime()))
+    {
+      throw CustomError(userDate + 'is not date format');
     }
   }
-  else {throw 'Unable to determine the time of year!';}
-  let month = userDate.getMonth();
-  console.log(month);
-  let season = '';
-  switch((month+1).toString()) {
-    case '12':
-    case '1':
-    case '2':
-      season = 'winter';
-      break;
-    case '3':
-    case '4':
-    case '5':
-      season = 'spring';
-      break;
-    case '6':
-    case '7':
-    case '8':
-      season = 'summer';
-      break;
-    case '9':
-    case '10':
-    case '11':
-      season = 'fall';
-      break;
+  else
+    {
+      throw CustomError('Unable to determine the time of year!');
   }
-  return season;
+
+  let month = userDate.getMonth();
+  let winter = 'winter';
+  let spring = 'spring';
+  let summer = 'summer';
+  let fall = 'fall';
+  let seasons = {
+    '12':winter,
+    '1': winter,
+    '2': winter,
+    '3': spring,
+    '4': spring,
+    '5': spring,
+    '6': summer,
+    '7': summer,
+    '8': summer,
+    '9': fall,
+    '10': fall,
+    '11': fall
+  };
+  return seasons[(month+1).toString()];
 };
 
