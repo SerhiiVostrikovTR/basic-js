69421ecbf1939543c92f9fe92422a269ffb9a47e
serhii_vostrikov <serhii.vostrikov@thomsonreuters.com>
2020-12-09 12:59:26 +0200

    Refactor count-cats according to PRs comments

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,10 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(arrayTest) {
-  let counter = 0;
-  for(let i=0; i<arrayTest.length; i++){
-
-    counter += arrayTest[i].filter(cats => cats === '^^').length;
-  }
-  return counter
+  return arrayTest.reduce((acc, item) => {
+    return acc + item.filter(cats => cats === '^^').length;
+  },0)
 };
