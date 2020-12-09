3531b942acdbb447b795aea51b09528f911118a9
serhii_vostrikov <serhii.vostrikov@thomsonreuters.com>
2020-12-09 15:56:25 +0200

    Refactor dream-team according to PRs comments

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(teamMembersArray) {
-  let teamName = '';
-  let teamCheck = '';
   if(!Array.isArray(teamMembersArray)){
     return false;
   }
-  teamCheck = teamMembersArray.filter(name => typeof(name) == "string" );
-  for(let i=0; i<teamCheck.length; i++){
-    teamName += teamCheck[i].replace(/\s/g, '').charAt(0).toUpperCase();
-  }
-  return teamName.split('').sort().join('');
+  return teamMembersArray.reduce((acc, name) =>
+      typeof name == 'string' ? [...acc, name.trim().charAt(0).toUpperCase()] : acc, [])
+      .sort()
+      .join('');
 };
