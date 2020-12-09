ed9bc3884500628c16a4d584a1adcacab299348f
Serhii_Vostrikov <svostrikov.contractor@libertyglobal.com>
2020-12-10 00:47:48 +0200

    refactor extended-repeater. Fixed default value for 'additionSeparator'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,8 +2,14 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(repeatString, repeatOptions) {
   const basicRepeater = (str, repeatNumber, strSeparator='+') => {return (str+strSeparator).repeat(repeatNumber-1).concat(str)}
-  const additionRepeaterStr =  basicRepeater(repeatOptions.addition, repeatOptions.additionRepeatTimes, repeatOptions.additionSeparator);
-  return basicRepeater(repeatString+(additionRepeaterStr==='undefined' ? '':additionRepeaterStr), repeatOptions['repeatTimes'], repeatOptions['separator']);
+  const additionRepeaterStr =  basicRepeater(
+      repeatOptions.addition,
+      repeatOptions.additionRepeatTimes,
+      repeatOptions.additionSeparator ? repeatOptions.additionSeparator : '|');
+  return basicRepeater(
+      repeatString+(additionRepeaterStr==='undefined' ? '':additionRepeaterStr),
+      repeatOptions['repeatTimes'],
+      repeatOptions['separator']);
 };
 
   
