dc90c775fb9e83c46ea9b17a8743ce06df500f87
serhii_vostrikov <serhii.vostrikov@thomsonreuters.com>
2020-12-09 11:35:52 +0200

    Refactor hanoi-tower according to PR comments

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let turns = 2**disksNumber - 1;
-  let seconds = 3600/turnsSpeed*turns;
-  return {'turns': turns, 'seconds': Math.floor(seconds)};
+  const turns = 2**disksNumber - 1;
+  const seconds = Math.floor(3600/turnsSpeed*turns);
+  return {turns, seconds};
 };
+
+
 
 
