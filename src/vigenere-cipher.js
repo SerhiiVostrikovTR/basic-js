c7873f462b58075ad6bc0503a1c6556bfe10b317
serhii_vostrikov <serhii.vostrikov@thomsonreuters.com>
2020-12-10 15:25:56 +0200

    Refactoring vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,62 +1,55 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 
 class VigenereCipheringMachine {
   constructor(directMachine=true) {
     this.directMachine = directMachine;
   }
 
-  ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-  getAlphabetValueByIndex = index => this.ALPHABET[index];
-  getAlphabetIndexByValue = value => this.ALPHABET.indexOf(value);
-  secretWordMultiple = (stringToModify, secretKey) => {return secretKey.repeat((stringToModify.length/secretKey.length)+1).substring(0,stringToModify.length-1)}
+  secretWordMultiple = (stringToModify, secretKey) =>
+    secretKey.repeat((stringToModify.length/secretKey.length)+1).substring(0,stringToModify.length)
 
-  commonEncryptDecryptSteps(stringToModify, secretKey, encrypt=true){
-    let modifiedStringArray = [];
+
+  pairCharsStringAndSecret(stringToModify, secretKey){
     let secretKeyIndexCounter = 0;
-    const alphabetLength = this.ALPHABET.length;
-    const secretNew = this.secretWordMultiple(stringToModify, secretKey)
-    stringToModify = stringToModify.toUpperCase();
-    secretKey = secretKey.toUpperCase();
-
-    for(let i=0; i<stringToModify.length; i++){
-      let modifiedChar = stringToModify[i];
-      if(this.ALPHABET.includes(stringToModify[i])) {
-        // let currentCharFromString = stringToModify.charAt(i);
-        let currentIndexFromCharOfStringInAlphabet = this.getAlphabetIndexByValue(stringToModify.charAt(i));
-        // let currentCharFromSecretString = secretKey.charAt(secretKeyIndexCounter % secretKey.length);
-        let currentIndexFromCharOfSecretWordInAlphabet = this.getAlphabetIndexByValue(secretKey.charAt(secretKeyIndexCounter % secretKey.length));
-        modifiedChar = encrypt ? this.getAlphabetValueByIndex((currentIndexFromCharOfStringInAlphabet +
-            currentIndexFromCharOfSecretWordInAlphabet) % alphabetLength) : this.getAlphabetValueByIndex((currentIndexFromCharOfStringInAlphabet -
-            currentIndexFromCharOfSecretWordInAlphabet + alphabetLength) % alphabetLength);
-        secretKeyIndexCounter++;
-      }
-      modifiedStringArray.push(modifiedChar);
-    }
-    return modifiedStringArray;
+    return stringToModify.map(char => {
+      return ALPHABET.includes(char) ? [char, secretKey[secretKeyIndexCounter++]] : [char, null];
+    });
   }
+
+  commonEncryptDecryptFunction = (stringToModify, secretKey, encrypt=true) =>
+      this.pairCharsStringAndSecret(
+          stringToModify.toUpperCase().split(''),
+          this.secretWordMultiple(stringToModify, secretKey).toUpperCase().split(''))
+          .reduce((acc, currChar) =>
+              {
+                if(ALPHABET.includes(currChar[0])) {
+                  if(encrypt)
+                  {
+                    return acc + ALPHABET[(ALPHABET.indexOf(currChar[0]) + ALPHABET.indexOf(currChar[1])) % ALPHABET.length];
+                  }
+                  return acc + ALPHABET[(ALPHABET.indexOf(currChar[0]) - ALPHABET.indexOf(currChar[1]) + ALPHABET.length) % ALPHABET.length];
+                }
+                return acc+currChar[0];
+              }
+              , '');
 
   encrypt(stringToEncrypt, secretKey) {
     if (arguments.length!==2) {throw CustomError('Invalid number of arguments!');}
-    let encryptedMessage = this.commonEncryptDecryptSteps(stringToEncrypt, secretKey);
-    return this.directMachine ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+    let encryptedMessage = this.commonEncryptDecryptFunction(
+        stringToEncrypt,
+        secretKey);
+    return this.directMachine ? encryptedMessage : encryptedMessage.split('').reverse().join('');
   }
   decrypt(stringToDecrypt, secretKey) {
     if (arguments.length!==2) {throw CustomError('Invalid number of arguments!');}
-    let decryptedMessage = this.commonEncryptDecryptSteps(stringToDecrypt, secretKey, false);
-    return this.directMachine ? decryptedMessage.join('') : decryptedMessage.reverse().join('');
+    let decryptedMessage = this.commonEncryptDecryptFunction(
+        stringToDecrypt,
+        secretKey,
+        false);
+    return this.directMachine ? decryptedMessage : decryptedMessage.split('').reverse().join('');
   }
 }
 
 module.exports = VigenereCipheringMachine;
-
-
-
-a = new VigenereCipheringMachine();
-console.log(a.encrypt('attack at dawn!', 'alphonse'))
-
-// console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }))
-// 'nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null');
-// console.log(repeater('la', { repeatTimes: 3 }))
-// 'la+la+la')
